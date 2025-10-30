e5619383046798a404574efb5a4f8fa4c32b4330
Monika Stoyanova <mstoyanova@devexperts.com>
2025-10-30 18:53:43 +0200

    Initial Commit

---
+++ SF Users/SF_UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SF_UsersApp.swift
+//  SF Users
+//
+//  Created by Monika Stoyanova on 30.10.25.
+//
+
+import SwiftUI
+
+@main
+struct SF_UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
